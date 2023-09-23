3f5c6afe09d404a8792c482a0d5c3696d0e245f5
Marynryn <murmar95@gmail.com>
2023-09-23 20:29:55 +0300

    1 та 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const bodyEl = document.querySelector("body");
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+let timerId = null;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+
+  }
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    let colorValue = getRandomHexColor()
+    bodyEl.style.backgroundColor = colorValue;
+    
+  }, 1000);
+  startBtn.setAttribute("disabled", true);
+}
+
+);
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute("disabled");
+
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,7 +1,18 @@
+const formEl = document.querySelector(".form");
+const delayEl = document.getElementsByName("delay");
+const starEl = document.getElementsByName("step");
+const amountEl = document.getElementsByName("amount");
+const btn = document.querySelector("button");
+
+formEl.addEventListener("submit", amountEl * createPromise);
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    const timerId = setTimeout(() => {
+      console.log("I love async JS!");
+    }, delayEl);
   } else {
     // Reject
   }
