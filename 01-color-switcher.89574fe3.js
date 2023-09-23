992024934dece64b3813dea925a3c01d4d45fc8f
Marynryn <132609093+Marynryn@users.noreply.github.com>
2023-09-23 17:30:23 +0000

    Deploying to gh-pages from @ Marynryn/goit-js-hw-09@3f5c6afe09d404a8792c482a0d5c3696d0e245f5 🚀

---
+++ 01-color-switcher.89574fe3.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=null;e.addEventListener("click",(function(){o=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));t.style.backgroundColor=e}),1e3),e.setAttribute("disabled",!0)})),n.addEventListener("click",(function(){clearInterval(o),e.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.89574fe3.js.map
---
+++ 03-promises.6b520779.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".form"),t=document.getElementsByName("delay"),n=(document.getElementsByName("step"),document.getElementsByName("amount"));document.querySelector("button");e.addEventListener("submit",n*function(e,n){if(Math.random()>.3)setTimeout((function(){console.log("I love async JS!")}),t)})}();
+//# sourceMappingURL=03-promises.6b520779.js.map
