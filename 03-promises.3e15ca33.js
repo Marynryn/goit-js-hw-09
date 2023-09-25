b1f0c06a90f70d2dd28c8e158344f138b0f159f9
Marynryn <132609093+Marynryn@users.noreply.github.com>
2023-09-25 17:14:45 +0000

    Deploying to gh-pages from @ Marynryn/goit-js-hw-09@bd5ef7f10cb44cf5c21ad985c58a0f605c161f52 🚀

--- 03-promises.3e15ca33.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form"),t=document.getElementsByName("delay"),n=(document.getElementsByName("step"),document.getElementsByName("amount"));document.querySelector("button");e.addEventListener("submit",n*function(e,n){if(Math.random()>.3){setTimeout((()=>{console.log("I love async JS!")}),t)}});
-//# sourceMappingURL=03-promises.3e15ca33.js.map
