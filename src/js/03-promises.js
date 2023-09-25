bd5ef7f10cb44cf5c21ad985c58a0f605c161f52
Marynryn <murmar95@gmail.com>
2023-09-25 20:14:16 +0300

    update 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,19 +1,43 @@
-const formEl = document.querySelector(".form");
-const delayEl = document.getElementsByName("delay");
-const starEl = document.getElementsByName("step");
-const amountEl = document.getElementsByName("amount");
-const btn = document.querySelector("button");
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-formEl.addEventListener("submit", amountEl * createPromise);
+const formEl = document.querySelector(".form");
+
+
+formEl.addEventListener("submit", submitForm);
+
 
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+  const shouldResolve = Math.random() > 0.3; 
+  return new Promise((resolve, reject) => {
+  setTimeout(() =>{
   if (shouldResolve) {
-    const timerId = setTimeout(() => {
-      console.log("I love async JS!");
-    }, delayEl);
-  } else {
-    // Reject
+     resolve(position, delay);}
+     else{
+      reject(position, delay); }
+    }, delay);
+
+});}
+
+
+
+
+
+function submitForm (event){
+  // event.preventDefault;
+  let delay  = formEl.delay.value
+  console.log(formEl.amount.value)
+  for (let i = 1; i <= formEl.amount.value; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay +=  formEl.step.value;
   }
 }
+
+
+
